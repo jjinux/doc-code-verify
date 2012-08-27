988e1466eea72428b12dd93770cdaa90af286335
Shannon -jj Behrens <jjinux@gmail.com>
2012-08-27 11:17:17 +0200

    Implemented the code to scan for examples

--- doc_code_merge.dart
+++ doc_code_merge.dart
@@ -1,10 +1,38 @@
 #library("doc_code_merge");
 
-// This is just a test.
-num add(num a, num b) {
-  return a + b;
-}
-
-void main() {
-  print("Hello, World!");
+class DocCodeMerger {
+  static final newlineRegExp = const RegExp(@"\r\n|\r|\n");
+  static final beginRegExp = const RegExp(@"#BEGIN +(\w+)");
+  static final endRegExp = const RegExp(@"#END +(\w+)");
+  static final newline = "\n";
+  
+  Map<String, StringBuffer> examples;
+  
+  DocCodeMerger() {
+    examples = new Map<String, StringBuffer>();
+  }
+  
+  /// Scan input for examples and update `examples`.
+  void scanForExamples(String input) {
+    List<String> lines = input.split(newlineRegExp);
+    String exampleName;
+    lines.forEach((line) {
+      Match beginMatch = beginRegExp.firstMatch(line);
+      if (beginMatch != null) {
+        exampleName = beginMatch[1];
+        return;
+      }
+      
+      Match endMatch = endRegExp.firstMatch(line);
+      if (endMatch != null) {
+        exampleName = null;
+        return;
+      }
+      
+      if (exampleName != null) {
+        examples.putIfAbsent(exampleName, () => new StringBuffer());
+        examples[exampleName].addAll([line, newline]);
+      }
+    });
+  }
 }
--- test_doc_code_merge.dart
+++ test_doc_code_merge.dart
@@ -3,7 +3,35 @@
 #import('doc_code_merge.dart');
 
 void main() {
-  // This is just a test of a test :)
-  test('add', () =>
-    expect(add(2, 2), 4));
+  group('DocCodeMerger', () {
+    DocCodeMerger merger;
+    
+    setUp(() {
+      merger = new DocCodeMerger();
+    });
+    
+    test('examples is empty by default', () {
+      expect(merger.examples.isEmpty(), isTrue);
+    });
+    
+    test('scanForExamples updates examples', () {      
+      merger.scanForExamples("""
+        // #BEGIN add
+        num add(num a, num b) {
+          return a + b;
+        }
+        // #END add
+        
+        void main() {
+          print("Hello, World!");
+        }
+      """);
+      
+      expect(merger.examples["add"].toString().trim(), equals("""
+        num add(num a, num b) {
+          return a + b;
+        }
+      """.trim()));
+    });
+  });
 }
